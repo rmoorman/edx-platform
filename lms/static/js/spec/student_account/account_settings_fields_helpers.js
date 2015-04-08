6254bead9219e197257067c27cfa87fd16950a98
cahrens <christina@edx.org>
2015-04-08 13:00:34 -0400

    Add change_initiated eventing for accounts page.

--- lms/static/js/spec/student_account/account_settings_fields_helpers.js
+++ lms/static/js/spec/student_account/account_settings_fields_helpers.js
@@ -1,17 +1,29 @@
 define(['backbone', 'jquery', 'underscore', 'js/common_helpers/ajax_helpers', 'js/common_helpers/template_helpers',
-        'js/spec/views/fields_helpers',
+        'js/spec/views/fields_helpers', 'logger',
         'string_utils'],
-    function (Backbone, $, _, AjaxHelpers, TemplateHelpers, FieldViewsSpecHelpers) {
+    function (Backbone, $, _, AjaxHelpers, TemplateHelpers, FieldViewsSpecHelpers, Logger) {
         'use strict';
 
-        var verifyAuthField = function (view, data, requests) {
-            var selector = '.u-field-value > a';
+        var verifyAuthField = function (view, data, requests, event_user_id) {
+            var selector = '.u-field-value > a', eventData = {};
 
             spyOn(view, 'redirect_to');
 
             FieldViewsSpecHelpers.expectTitleAndMessageToBe(view, data.title, data.helpMessage);
             expect(view.$(selector).text().trim()).toBe('Unlink');
             view.$(selector).click();
+
+            if (event_user_id) {
+                eventData[data.valueAttribute] = {'old_value': 'connected', 'new_value': 'disconnected'};
+                expect(Logger.log).toHaveBeenCalledWith(
+                    "edx.user.settings.change_initiated",
+                    {'user_id': event_user_id, 'settings': eventData}
+                );
+            }
+            else {
+                expect(Logger.log).not.toHaveBeenCalled();
+            }
+
             FieldViewsSpecHelpers.expectMessageContains(view, 'Unlinking');
             AjaxHelpers.expectRequest(requests, 'POST', data.disconnectUrl);
             AjaxHelpers.respondWithNoContent(requests);
@@ -20,9 +32,21 @@ define(['backbone', 'jquery', 'underscore', 'js/common_helpers/ajax_helpers', 'j
             FieldViewsSpecHelpers.expectMessageContains(view, 'Successfully unlinked.');
 
             view.$(selector).click();
+
+            if (event_user_id) {
+                eventData[data.valueAttribute] = {'old_value': 'disconnected', 'new_value': 'connected'};
+                expect(Logger.log).toHaveBeenCalledWith(
+                    "edx.user.settings.change_initiated",
+                    {'user_id': event_user_id, 'settings': eventData}
+                );
+            }
+            else {
+                expect(Logger.log).not.toHaveBeenCalled();
+            }
+
             FieldViewsSpecHelpers.expectMessageContains(view, 'Linking');
             expect(view.redirect_to).toHaveBeenCalledWith(data.connectUrl);
-        }
+        };
 
         return {
             verifyAuthField: verifyAuthField
